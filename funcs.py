d6e06dc6e43cb89b5a817fcc34dbe40908590ded
yjb23tech <yjb23tech@gmail.com>
2023-09-21 18:16:55 +0100

    LOL

--- funcs.py
+++ funcs.py
@@ -1,8 +1,10 @@
 #print("Gunna Gunna Gunna")
 
 def pvp(player_x, player_y, round_counter):
-    
+
     print(f"Round {round_counter + 1}: FIGHT!")
     print(f"{player_x.str_player_name} has an attack power of {player_x.int_launch_atk()}")
     print(f"{player_y.str_player_name} has a defensive power of {player_y.int_launch_def()}\n")
+
+    
 
