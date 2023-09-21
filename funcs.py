c8e947bed5b7597b2f404ee53b1dd742364a021a
yjb23tech <yjb23tech@gmail.com>
2023-09-21 17:16:15 +0100

    Experimenting

--- funcs.py
+++ funcs.py
@@ -1,1 +1,6 @@
-print("Gunna Gunna Gunna")
+#print("Gunna Gunna Gunna")
+
+def pvp(player_x, player_y):
+    print(f"{player_x.str_player_name} has an attack power of {player_x.int_launch_atk()}")
+    print(f"{player_y.str_player_name} has a defensive power of {player_y.int_launch_def()}\n")
+
