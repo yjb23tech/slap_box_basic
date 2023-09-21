bfa68a86b5f8af3ec0571a93f5a174b5634056ae
yjb23tech <yjb23tech@gmail.com>
2023-09-21 16:08:12 +0100

    Working in VS Code makes me feel like a reall SWE hahaha

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import random
+
+class Player:
+
+    def __init__(self, str_player_name, str_player_location, int_player_age):
+        self.str_player_name = str_player_name
+        self.str_player_location = str_player_location
+        self.int_player_age = int_player_age
+
+    def __str__(self):
+        return (f"Please welcome to the stage {self.str_player_name} from {self.str_player_location}")
+
+test_player = Player("Yinka Banjo", "London", 29)
+
 print("Hello New World")
+print(test_player)
+
