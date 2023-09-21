09d2fafc917508ee2503b04eb223f99b33f63a9f
yjb23tech <yjb23tech@gmail.com>
2023-09-21 17:16:31 +0100

    Further growth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import funcs
 
 class Player:
 
@@ -7,9 +8,21 @@ class Player:
         self.str_player_name = str_player_name
         self.str_player_location = str_player_location
         self.int_player_age = int_player_age
+    
+        self.int_player_hp = 100
+        self.int_player_atk_pwr = 1
+        self.int_player_def_pwr = 1
 
     def __str__(self):
         return (f"Bio: Name ==> {self.str_player_name} Location ==> {self.str_player_location} Age ==> {self.int_player_age}")
+
+    def int_launch_atk(self):
+        self.int_player_atk_pwr = random.randint(1, 20)
+        return self.int_player_atk_pwr 
+    
+    def int_launch_def(self):
+        self.int_player_def_pwr = random.randint(1,20)
+        return self.int_player_def_pwr
 
 #Meta methods
 def set_player_name():
@@ -36,7 +49,7 @@ def play():
 
     #Events that're looped to model actual gameplay
     while (fight_counter < 10):
-        print(f"Round {fight_counter + 1}: FIGHT!")
+        funcs.pvp(player_1, player_2)
         fight_counter += 1
     print("\nAnd the winner is...\n")
 
